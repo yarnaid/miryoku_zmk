111da61bd73a6e2781ca117593ad4e0769b9057e
yarnaid <yarnaid@gmail.com>
2024-11-17 12:24:39 +0400

    my conf

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,7 +3,6 @@
 
 #define XXX &none
 #define MIRYOKU_CLIPBOARD_MAC
-// #define MIRYOKU_KLUDGE_MOUSEKEYSPR
 
 
 #define MIRYOKU_LAYOUTMAPPING_KYRIA(\
@@ -16,5 +15,3 @@
 &kp LCTRL                K10            K11  K12  K13  K14                           K15  K16            K17            K18  K19  &kp SQT  \
 &mt LEFT_SHIFT CAPSLOCK  K20            K21  K22  K23  K24  T00  T01       T02  T03  K25  K26            K27            K28  K29  &kp ESCAPE  \
                                         &kp LG(LS(A))  K32  K33  K34       K35  K36  K37  &kp LG(SPACE)  &kp LA(LS(A))
-#define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 43 44
-#define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 45 46
