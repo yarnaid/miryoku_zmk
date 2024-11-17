3cbba854d7f2249db7d482c479777ab0b00cced5
yarnaid <yarnaid@gmail.com>
2024-11-17 13:18:47 +0400

    my conf

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -16,15 +16,16 @@
 #define XXX &none
 
 #if defined MIRYOKU_MAPPING_EXTENDED_THUMBS
-#define MIRYOKU_LAYERMAPPING_BASE(\
+#define MIRYOKU_LAYOUTMAPPING_KYRIA(\
      K00, K01, K02, K03, K04,                          K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,                          K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,                          K35, K36, K37, N38, N39 \
 )\
-&kp TAB                  K00            K01  K02  K03  K04                           K05  K06            K07            K08  K09  &kp BACKSPACE  \
-&kp LCTRL                K10            K11  K12  K13  K14                           K15  K16            K17            K18  K19  &kp SQT  \
-&mt LEFT_SHIFT CAPSLOCK  K20            K21  K22  K23  K24  T00  T01       T02  T03  K25  K26            K27            K28  K29  &kp ESCAPE  \
+XXX  K00  K01  K02  K03  K04                           K05  K06  K07  K08  K09  XXX  \
+XXX  K10  K11  K12  K13  K14                           K15  K16  K17  K18  K19  XXX  \
+XXX  K20  K21  K22  K23  K24  XXX  XXX       XXX  XXX  K25  K26  K27  K28  K29  XXX  \
+               XXX  XXX  K32  K33  K34       K35  K36  K37  XXX  XXX
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 43 44
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 45 46
 #else
