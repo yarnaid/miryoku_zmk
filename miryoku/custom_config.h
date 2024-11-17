75c5f2a06776010eedd0f57cf34dfb825345839f
yarnaid <yarnaid@gmail.com>
2024-11-17 12:31:32 +0400

    my conf

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,6 +5,8 @@
 #define MIRYOKU_CLIPBOARD_MAC
 
 
+#if !defined (MIRYOKU_LAYOUTMAPPING_KYRIA)
+
 #define MIRYOKU_LAYOUTMAPPING_KYRIA(\
      K00, K01, K02, K03, K04,                          K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, \
@@ -15,3 +17,9 @@
 &kp LCTRL                K10            K11  K12  K13  K14                           K15  K16            K17            K18  K19  &kp SQT  \
 &mt LEFT_SHIFT CAPSLOCK  K20            K21  K22  K23  K24  T00  T01       T02  T03  K25  K26            K27            K28  K29  &kp ESCAPE  \
                                         &kp LG(LS(A))  K32  K33  K34       K35  K36  K37  &kp LG(SPACE)  &kp LA(LS(A))
+
+#define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 43 44
+#define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 45 46
+
+
+#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_KYRIA
