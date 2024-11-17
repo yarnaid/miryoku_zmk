88874fa9f162abff9a4c741ac46a8e38af995175
yarnaid <yarnaid@gmail.com>
2024-11-17 13:49:14 +0400

    my conf

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -22,10 +22,10 @@
      K20, K21, K22, K23, K24,                          K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,                          K35, K36, K37, N38, N39 \
 )\
-&kp TAB                       K00  K01  K02  K03  K04                                K05            K06  K07  K08  K09  XXX  \
-&kp LCTRL                     K10  K11  K12  K13  K14                                K15            K16  K17  K18  K19  XXX  \
-&mt LEFT_SHIFT CAPSLOCK       K20  K21  K22  K23  K24  XXX  XXX       XXX            K25            K26  K27  K28  K29  XXX  \
-               &kp LG(LS(A))  XXX  K32  K33  K34       K35  K36  K37  &kp LG(SPACE)  &kp LA(LS(A))
+&kp TAB                  K00  K01  K02            K03  K04                                      K05  K06            K07            K08  K09  &kp BACKSPACE  \
+&kp LCTRL                K10  K11  K12            K13  K14                                      K15  K16            K17            K18  K19  &kp SQT  \
+&mt LEFT_SHIFT CAPSLOCK  K20  K21  K22            K23  K24  XXX  &studio_unlock       XXX  XXX  K25  K26            K27            K28  K29  &kp ESCAPE  \
+                                   &kp LG(LS(A))  XXX  K32  K33  K34                  K35  K36  K37  &kp LG(SPACE)  &kp LA(LS(A))
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 43 44
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 45 46
 #else
