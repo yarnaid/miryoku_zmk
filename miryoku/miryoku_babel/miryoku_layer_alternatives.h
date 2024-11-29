fcad2505c9c4b082bc25e4b17a03b4f8fc72f02b
yarnaid <yarnaid@gmail.com>
2024-11-29 13:38:36 +0400

    new keys

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -116,8 +116,8 @@ U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp M,             &kp C,             &kp 
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTY \
-&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             HT(NUMBER_1, U),             HT(NUMBER_2, I),             HT(NUMBER_3, O),             &kp P,             \
-U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
+&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             HT(NUMBER_1, U),   HT(NUMBER_2, I),   HT(NUMBER_3, O),   &kp P,             \
+U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    U_MT(&sl U_APP, G),             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
