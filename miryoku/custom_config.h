966397a9f32690c8aa1cf92a83cf38832be9b31f
yarnaid <yarnaid@gmail.com>
2025-01-06 17:20:41 +0400

    combos

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -32,34 +32,34 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT
 
 
 
-#if !defined (MIRYOKU_LAYOUTMAPPING_KYRIA)
+// #if !defined (MIRYOKU_LAYOUTMAPPING_KYRIA)
 
-#if defined MIRYOKU_MAPPING_EXTENDED_THUMBS
-#define MIRYOKU_LAYOUTMAPPING_KYRIA(\
-     K00, K01, K02, K03, K04,                          K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,                          K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,                          K35, K36, K37, N38, N39 \
-)\
-&kp TAB                  K00  K01  K02            K03  K04                                                K05  K06            K07            K08  K09  &kp BACKSPACE  \
-&kp LCTRL                K10  K11  K12            K13  K14                                                K15  K16            K17            K18  K19  &kp SQT  \
-&mt LEFT_SHIFT CAPSLOCK  K20  K21  K22            K23  K24  &kp LGUI  &studio_unlock       XXX  &kp RGUI  K25  K26            K27            K28  K29  &kp ESCAPE  \
-                                   &kp LG(LS(A))  XXX  K32  K33       K34                  K35  K36       K37  &kp LG(SPACE)  &kp LA(LS(A))
-#define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 43 44
-#define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 45 46
-#else
-#define MIRYOKU_LAYOUTMAPPING_KYRIA(\
-     K00, K01, K02, K03, K04,                          K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,                          K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,                          K35, K36, K37, N38, N39 \
-)\
-XXX            K00  K01  K02  K03  K04                                                                        K05  K06  K07  K08  K09  XXX  \
-&kp LCTRL      K10  K11  K12  K13  K14                                                                        K15  K16  K17  K18  K19  XXX  \
-XXX            K20  K21  K22  K23  K24  &kp LS(LC(LA(LG(NUMBER_1))))  &kp LS(LC(LA(LG(NUMBER_2))))          &kp LS(LC(LA(LG(NUMBER_3))))  XXX  K25  K26  K27  K28  K29  XXX  \
-               &sl U_APP  K32  K33  K34  XXX                                                                        XXX  K35  K36  K37  XXX
-#define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 42 43
-#define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 46 47
-#endif
+// #if defined MIRYOKU_MAPPING_EXTENDED_THUMBS
+// #define MIRYOKU_LAYOUTMAPPING_KYRIA(\
+//      K00, K01, K02, K03, K04,                          K05, K06, K07, K08, K09, \
+//      K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, \
+//      K20, K21, K22, K23, K24,                          K25, K26, K27, K28, K29, \
+//      N30, N31, K32, K33, K34,                          K35, K36, K37, N38, N39 \
+// )\
+// &kp TAB                  K00  K01  K02            K03  K04                                                K05  K06            K07            K08  K09  &kp BACKSPACE  \
+// &kp LCTRL                K10  K11  K12            K13  K14                                                K15  K16            K17            K18  K19  &kp SQT  \
+// &mt LEFT_SHIFT CAPSLOCK  K20  K21  K22            K23  K24  &kp LGUI  &studio_unlock       XXX  &kp RGUI  K25  K26            K27            K28  K29  &kp ESCAPE  \
+//                                    &kp LG(LS(A))  XXX  K32  K33       K34                  K35  K36       K37  &kp LG(SPACE)  &kp LA(LS(A))
+// #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 43 44
+// #define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 45 46
+// #else
+// #define MIRYOKU_LAYOUTMAPPING_KYRIA(\
+//      K00, K01, K02, K03, K04,                          K05, K06, K07, K08, K09, \
+//      K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, \
+//      K20, K21, K22, K23, K24,                          K25, K26, K27, K28, K29, \
+//      N30, N31, K32, K33, K34,                          K35, K36, K37, N38, N39 \
+// )\
+// XXX            K00  K01  K02  K03  K04                                                                        K05  K06  K07  K08  K09  XXX  \
+// &kp LCTRL      K10  K11  K12  K13  K14                                                                        K15  K16  K17  K18  K19  XXX  \
+// XXX            K20  K21  K22  K23  K24  &kp LS(LC(LA(LG(NUMBER_1))))  &kp LS(LC(LA(LG(NUMBER_2))))          &kp LS(LC(LA(LG(NUMBER_3))))  XXX  K25  K26  K27  K28  K29  XXX  \
+//                &sl U_APP  K32  K33  K34  XXX                                                                        XXX  K35  K36  K37  XXX
+// #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 42 43
+// #define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 46 47
+// #endif
 
 #endif
